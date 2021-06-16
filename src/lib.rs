264a544c00154d0ba57284435aa81ba85536bea6
Joe Fioti <jafioti@gmail.com>
2021-06-16 10:49:12 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use std::{collections::VecDeque};
+
+pub trait Len {
+    fn len(&self) -> usize;
+}
+
+// IMPLEMENTATIONS
+impl Len for str {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl Len for String {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T> Len for Vec<T> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T> Len for VecDeque<T> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T: ?Sized + Len> Len for Box<T> {
+    fn len(&self) -> usize {
+        self.as_ref().len()
+    }
+}
+
+impl<T> Len for [T] {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl Len for pyo3::PyAny {
+    fn len(&self) -> usize {
+        self.len().expect("Failed to get length!")
+    }
+}
+
+impl Len for &pyo3::PyAny {
+    fn len(&self) -> usize {
+        (*self).len().expect("Failed to get length!")
+    }
+}
+
+impl Len for serde_json::Value {
+    fn len(&self) -> usize {
+        self.to_owned().len()
+    }
+}
+
+impl Len for &serde_json::Value {
+    fn len(&self) -> usize {
+        (*self).len()
+    }
+}
