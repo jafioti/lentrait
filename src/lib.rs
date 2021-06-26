0096a155d72c717bcebe2c4bc50c0afccd90b519
Joe Fioti <jafioti@gmail.com>
2021-06-26 17:08:10 -0400

    Added implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque};
+use std::{collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque}, ffi::{CStr, CString, OsStr, OsString}, hash::Hash};
 
 pub trait Len {
     fn len(&self) -> usize;
@@ -68,5 +68,66 @@ impl Len for &serde_json::Value {
 impl Len for mako::vocab::Vocab {
     fn len(&self) -> usize {
         self.num_tokens as usize
+    }
+}
+
+impl<K: Eq + Hash, V> Len for HashMap<K, V> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<K: Eq + Hash> Len for HashSet<K> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T> Len for LinkedList<T> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl Len for CStr {
+    fn len(&self) -> usize {
+        self.as_ref().len()
+    }
+}
+
+impl Len for CString {
+    fn len(&self) -> usize {
+        self.as_ref().len()
+    }
+}
+
+impl Len for OsStr {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl Len for OsString {
+    fn len(&self) -> usize {
+        self.as_os_str().len()
+    }
+}
+
+
+impl<K: Ord, V> Len for BTreeMap<K, V> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T: Ord> Len for BTreeSet<T> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T: Ord> Len for BinaryHeap<T> {
+    fn len(&self) -> usize {
+        self.len()
     }
 }
